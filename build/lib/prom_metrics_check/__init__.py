d03b36fcdb1bfa8d1af6b485d52993b27aacdcc7
Steven Mellor <steven.mellor@dominodatalab.com>
2022-10-07 12:10:48 -0700

    base

--- build/lib/prom_metrics_check/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Top-level package for prom-metrics-check."""
-
-__author__ = """Container Solutions"""
-__email__ = 'cre@container-solutions.com'
-__version__ = '0.1.0'
--- prom_metrics_check/cli.py
+++ prom_metrics_check/cli.py
@@ -5,7 +5,7 @@ import argparse
 import logging
 
 from .prom_metrics_check import load_dashboard, get_all_metrics, \
-    check_exist_metrics, json_dashboard, json_dir_dashboards
+    check_exist_metrics
 
 
 logging.basicConfig(level='ERROR')
@@ -17,14 +17,11 @@ PROMETHEUS_DEFAULT_URL = 'http://localhost:9090'
 
 GRAFANA_URL = os.environ.get(
     'GRAFANA_URL', GRAFANA_DEFAULT_URL)
-#GRAFANA_KEY = os.environ.get(
-#    'GRAFANA_KEY')
-GRAFANA_KEY = "eyJrIjoidDYwT1N4cmhGSWFWTXE3RkwxSERrT29GeGtaUGZVQXAiLCJuIjoic3RldmVudGVzdCIsImlkIjoxfQ=="
-
+GRAFANA_KEY = os.environ.get(
+    'GRAFANA_KEY')
 PROMETHEUS_URLS = os.environ.get(
     'PROMETHEUS_URLS', PROMETHEUS_DEFAULT_URL).split(',')
 
-JSON_DIRECTORY = 'dashboards'
 
 def main(args=None):
     """Console script for prom_metrics_check."""
@@ -34,28 +31,16 @@ def main(args=None):
     parser.add_argument(
         '--grafana-url',
         metavar='grafana_url',
-        help='Set grafana url. Default value is {url}. Must be set with --grafana-key.'.format(
+        help='Set grafana url. Default value is {url}'.format(
             url=GRAFANA_DEFAULT_URL),
         nargs='?',
         default=GRAFANA_URL)
     parser.add_argument(
         '--grafana-key',
         metavar='grafana_key',
-        help='Set grafana key to have API access. Must be set with --grafana-url',
+        help='Set grafana key to have API access.',
         nargs='?',
         default=GRAFANA_KEY)
-    parser.add_argument(
-        '--json-file',
-        metavar='json_file',
-        help='Set json file to Grafana dashboard.',
-        nargs='?')
-    parser.add_argument(
-        '--json-dir',
-        metavar='json_dir',
-        help='Set json directory for Grafana dashboards. Default is /{directory}'.format(
-            directory=JSON_DIRECTORY),
-        nargs='?',
-        default=JSON_DIRECTORY)
     parser.add_argument(
         '--prometheus-urls',
         metavar='prometheus_urls',
@@ -64,11 +49,8 @@ def main(args=None):
         nargs='*',
         default=PROMETHEUS_URLS)
     args = parser.parse_args(args)
-    print(args)
     for url in args.prometheus_urls:
-       # dashboards = load_dashboard(url=args.grafana_url, key=args.grafana_key) if 
-        #dashboards = json_dashboard(json_file=args.json_file)
-        dashboards = json_dir_dashboards(directory=args.json_dir)
+        dashboards = load_dashboard(url=args.grafana_url, key=args.grafana_key)
         except_metrics = get_all_metrics(dashboards=dashboards)
         missing_metrics = check_exist_metrics(except_metrics, url)
         if missing_metrics:
